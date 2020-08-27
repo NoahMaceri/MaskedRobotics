64d7e1dbeaa4557bd431307091fc616db4e4c4d6
Noah Maceri <noah.maceri@gmail.com>
2020-08-27 13:58:45 -0400

    Develop (#11)
    * Added initial HomonomicDrive
    
    Trey asked me nicely
    
    Co-Authored-By: Trey Sprecher <53835542+tsprech@users.noreply.github.com>
    
    * Commited HolonomicDrive code, untested
    
    * Updated gear ratio to 36_1. Also added project desc
    
    * Added inital claw and intake control
    
    * Working Intake and Arm Motors
    
    Fixed the issues with getting the arm and intake to work with the controller.
    
    * Inital push, please Trey cry me a river
    
    * Fixed an issue
    
    * Brakes for the claw
    
    * Added an auton timeout
    
    * Added baseball auton
    
    * Added method for baseball auton to organize code
    
    * Tweaked Baseball auton for no penalties
    
    Co-authored-by: Trey Sprecher <53835542+tsprech@users.noreply.github.com>
    Co-authored-by: TSprech <trey.sprecher@gmail.com>
    Co-authored-by: Noah Maceri <noahmaceri@Noahs-MacBook-Pro.local>
    Co-authored-by: Noah Maceri <noahmaceri@MacBook-Pro.localdomain>

---
+++ TheCode/include/robot-config.h
@@ -0,0 +1,20 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor Motor1;
+extern motor Motor10;
+extern motor Motor11;
+extern motor Motor20;
+extern motor Motor5;
+extern motor Motor6;
+extern motor29 Motor393A;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
